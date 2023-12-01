07ed8c4df113b5404234a7d05427b8afc87b010a
AllysonH2o <allyson.araujo.h2o@gmail.com>
2023-12-01 16:10:54 -0300

    Conexão banco

--- App_Agendei/Back/PHP/cadastro.php
+++ App_Agendei/Back/PHP/cadastro.php
@@ -1,5 +1,5 @@
 <?php 
-
+require 'connect.php';
 
 if(isset($_POST['name']) && !empty($_POST['name']) && isset($_POST['email']) && !empty($_POST['email']) && isset($_POST['senha']) && !empty($_POST['senha'])){
 
@@ -12,10 +12,27 @@ $senha = addslashes($_POST['senha']);
     header("Location: App_Agendei\Views\cadastro.html");
     
 }
+//cadastro no banco começa aqui
+$nome = $_POST['nome'];
+$email = $_POST['email'];
+$senha = $_POST['senha'];
 
+//chegar se o email já esta cadastrado
+$sql = "SELECT * FROM usuario WHERE email = '$email'";
+$result = mysqli_query($conn, $sql);
 
+if (mysqli_num_rows($result)>0){
+    header('location: ..\..\Views\cadastro.html');
+}else{
+    //gravando no banco
+    $sql1 = "INSERT INTO usuario (nome, email, senha) VALUES ('$nome', '$email', '$senha')";
+    $result1 = mysqli_query($conn, $sql1);
 
-
-
+    if($result1){
+        header('location: ..\..\Views\perfil.html');
+    }else{
+        header('location: ..\..\Views\cadastro.html');
+    }
+}
 
 ?>
